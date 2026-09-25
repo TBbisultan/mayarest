6a9e814897a9f16c73b8eb8786a2d4cab40984e8
TBbisultan <btazoi@mail.ru>
2026-09-25 20:19:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ const ITEMS = [
 // ВТОРЫЕ БЛЮДА
 // =====================================================
 
-{ id:'m1', cat:'main', name:'Цыплёнок из печи с запечённым картофелем', desc:'', price:3890, img:'images/chicken-breast-with-mushroom-saute.jpg' },
+{ id:'m1', cat:'main', name:'Куринная грудка с грибным соте', desc:'', price:3890, img:'images/chicken-breast-with-mushroom-saute.jpg' },
 { id:'m2', cat:'main', name:'Мясо по-тайски', desc:'', price:3790, img:'images/myaso.po.tayski.jpg' },
 { id:'m3', cat:'main', name:'Стейк Рибай', desc:'', price:7890, img:'images/ribay.steyk.jpg' },
 { id:'m4', cat:'main', name:'Строганов с картофельным пюре', desc:'', price:3890, img:'images/befstroganov.s.pure.jpg' },
@@ -100,32 +100,14 @@ const ITEMS = [
 { id:'m8', cat:'main', name:'Куриная грудка с грибным соусом', desc:'', price:3890, img:'images/chicken-breast-with-mushroom-saute.jpg' },
 { id:'m9', cat:'main', name:'Тушёнка с сырными макаронами', desc:'', price:4690, img:null },
 
-{ id:'m10', cat:'main', name:'Чунцин Лазжи', desc:'重庆辣子鸡', price:0, img:null },
-{ id:'m11', cat:'main', name:'Синьцзян Лазжи', desc:'新疆辣子鸡', price:0, img:null },
+{ id:'m10', cat:'main', name:'Чунцин Лазжи', desc:'重庆辣子鸡', price:7690, img:'images/chongqing-laziji.jpg' },
+{ id:'m11', cat:'main', name:'Синьцзян Лазжи', desc:'新疆辣子鸡', price:6990, img:'images/xinjiang-laziji.jpg' },
 { id:'m12', cat:'main', name:'Дапаньцзи', desc:'大盘鸡', price:0, img:'images/dapanji.jpg' },
-{ id:'m13', cat:'main', name:'Чжунпаньцзи', desc:'中盘鸡', price:0, img:null },
-{ id:'m14', cat:'main', name:'Дапань с бараниной', desc:'大盘羊肉', price:0, img:null },
-{ id:'m15', cat:'main', name:'Баранина на железной плите', desc:'铁板羊排', price:0, img:null },
-{ id:'m16', cat:'main', name:'Мясо с зелёным луком', desc:'葱爆肉', price:0, img:null },
-{ id:'m17', cat:'main', name:'Жареный картофель с мясом', desc:'薯条炒肉', price:0, img:null },
-{ id:'m18', cat:'main', name:'Овощи с фунчозой и мясом', desc:'百菜粉条肉', price:0, img:null },
-{ id:'m19', cat:'main', name:'Мясо, жаренное с перцем', desc:'辣椒炒肉', price:0, img:null },
-{ id:'m20', cat:'main', name:'Мала сянго', desc:'麻辣香锅', price:0, img:null },
-{ id:'m21', cat:'main', name:'Жареная пекинская капуста', desc:'素炒上海心', price:0, img:null },
-{ id:'m22', cat:'main', name:'Жареные грибы', desc:'干炸蘑菇', price:0, img:null },
-{ id:'m23', cat:'main', name:'Брокколи с чесноком', desc:'蒜蓉西兰花', price:0, img:null },
-{ id:'m24', cat:'main', name:'Острый тофу', desc:'麻辣豆腐', price:0, img:null },
-{ id:'m25', cat:'main', name:'Жареная стручковая фасоль', desc:'干煸豆角', price:0, img:null },
-{ id:'m26', cat:'main', name:'Домашний тофу', desc:'家常豆腐', price:0, img:null },
-{ id:'m27', cat:'main', name:'Тофу в соусе Хуншао', desc:'红烧豆腐', price:0, img:null },
-{ id:'m28', cat:'main', name:'Жареные древесные грибы', desc:'素炒木耳', price:0, img:null },
-{ id:'m29', cat:'main', name:'Кунг Пао с курицей', desc:'宫保鸡丁', price:0, img:null },
-{ id:'m30', cat:'main', name:'Говядина на железной плите', desc:'铁板牛肉', price:0, img:null },
-{ id:'m31', cat:'main', name:'Курица на железной плите', desc:'铁板鸡片', price:0, img:null },
-{ id:'m32', cat:'main', name:'Рыба в соусе Хуншао', desc:'红烧鱼', price:0, img:null },
-{ id:'m33', cat:'main', name:'Жареный карп', desc:'干炸鲤鱼', price:0, img:null },
-{ id:'m34', cat:'main', name:'Куриные крылышки в соусе Кола', desc:'可乐鸡翅', price:0, img:null },
-{ id:'m35', cat:'main', name:'Острый жареный говяжий желудок', desc:'爆炒牛肚', price:0, img:null },
+{ id:'m19', cat:'main', name:'Лажау Чауру', desc:'辣椒炒肉', price:4190, img:'images/lajiao-chaorou.jpg' },
+{ id:'m25', cat:'main', name:'Ганьбянь Дужау', desc:'干煸豆角', price:4790, img:'images/ganbian-doujiao.jpg' },
+{ id:'m29', cat:'main', name:'Гунбао Жидин', desc:'宫保鸡丁', price:3290, img:'images/gongbao-jiding.jpg' },
+{ id:'m30', cat:'main', name:'Тебань Ру', desc:'铁板牛肉', price:5990, img:'images/tieban-niurou.jpg' },
+{ id:'m32', cat:'main', name:'Хуншау Юй', desc:'红烧鱼', price:11990, img:'images/hongshao-yu.jpg' },
 
 
 // =====================================================
